6db7dae5831837527ac27b4c3d2c45c1eee19707
Utkarsh Maharana <utkarshm27@gmail.com>
2020-05-23 23:04:52 +0530

    declare exact path

--- src/navigator/AppNavigator.tsx
+++ src/navigator/AppNavigator.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "../containers/Landing/Landing";
 import Courses from "../containers/Courses/Courses";
 export default function AppNavigator() {
   return (
-    <Router>
-      <div>
+    <div>
+      <Router>
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Landing />
           </Route>
           <Route path="/courses">
             <Courses />
           </Route>
         </Switch>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
