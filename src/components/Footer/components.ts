fa55e75b5f8f9dfc13f3f097422ba0a596e761fc
Utkarsh Maharana <utkarshm27@gmail.com>
2020-05-25 21:26:38 +0530

    footer component

---
+++ src/components/Footer/components.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  margin-top: 6.063rem;
+  background-image: linear-gradient(82deg, #207cee -3%, #73caf3 119%);
+  width: 100%;
+  padding-top: 25px;
+  padding-bottom: 22px;
+`;
+
+export const FooterText = styled.p`
+  margin: 0px;
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 2.2;
+  letter-spacing: 0.11px;
+  text-align: center;
+  color: #ffffff;
+`;
