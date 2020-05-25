313c7dabc773f2c96fac98fc233396a796d83c1e
Utkarsh Maharana <utkarshm27@gmail.com>
2020-05-25 08:03:19 +0530

    Carousel Component

---
+++ src/components/CarouselCard/CarouselCard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Card } from "antd";
+import styled from "styled-components";
+
+type Props = {
+  imageLink: string;
+  text: string;
+};
+export default function CarouselCard(props: Props) {
+  const { imageLink, text } = props;
+  return (
+    <CardBody hoverable>
+      <img
+        src={imageLink}
+        alt={text}
+        style={{ padding: "1.5rem 3.125rem", height: "108px", width: "108px" }}
+      />
+      <CardText>{text}</CardText>
+    </CardBody>
+  );
+}
+
+const CardText = styled.h1`
+  font-family: Montserrat;
+  font-size: 20px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.4;
+  letter-spacing: 0.2px;
+  text-align: center;
+  margin: 0px;
+  padding: 0rem 1.5rem 1.5rem 1.5rem;
+`;
+
+const CardBody = styled(Card)`
+  background-color: #ffffff;
+  width: 208px;
+  border: solid 1px #ebebeb;
+  border-radius: 0.25rem;
+
+  .ant-card-body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+  }
+`;
---
+++ src/components/LandingCarousel/Carousel.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { CarouselHeader, NavButton, CarouselSlider } from "./components";
+import CarouselCard from "../CarouselCard/CarouselCard";
+import webDevIcon from "../../assets/images/landing/web-dev-logo.svg";
+import mobAppIcon from "../../assets/images/landing/mobapp-dev-logo.svg";
+import arVrIcon from "../../assets/images/landing/ar-vr-logo.svg";
+import mlIcon from "../../assets/images/landing/ml-logo.svg";
+import leftArr from "../../assets/images/landing/left.svg";
+import rightArr from "../../assets/images/landing/right.svg";
+
+const NextArrow = (props: any) => {
+  const { onClick } = props;
+  return (
+    <NavButton onClick={onClick}>
+      <img src={rightArr} alt="next" />{" "}
+    </NavButton>
+  );
+};
+
+const PrevArrow = (props: any) => {
+  const { onClick } = props;
+  return (
+    <NavButton onClick={onClick}>
+      <img src={leftArr} alt="prev" />{" "}
+    </NavButton>
+  );
+};
+
+let settings = {
+  dots: false,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  initialSlide: 0,
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+
+export default function LandingCarousel() {
+  return (
+    <div style={{ marginTop: "5.25rem" }}>
+      <CarouselHeader>What do you want to learn?</CarouselHeader>
+      <CarouselSlider {...settings}>
+        <CarouselCard imageLink={webDevIcon} text="Web Development" />
+        <CarouselCard imageLink={mobAppIcon} text="Mobile App Development" />
+        <CarouselCard imageLink={arVrIcon} text="Mixed Reality (AR/VR)" />
+        <CarouselCard imageLink={mlIcon} text="Machine Learning (AI)" />
+        <CarouselCard imageLink={webDevIcon} text="Web Development" />
+        <CarouselCard imageLink={mobAppIcon} text="Mobile App Development" />
+        <CarouselCard imageLink={arVrIcon} text="Mixed Reality (AR/VR)" />
+        <CarouselCard imageLink={mlIcon} text="Machine Learning (AI)" />
+      </CarouselSlider>
+    </div>
+  );
+}
