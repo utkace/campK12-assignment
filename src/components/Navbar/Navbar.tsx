19e8b162538db4fd53ab9539ed6a44f1caa94c46
Utkarsh Maharana <utkarshm27@gmail.com>
2020-05-26 06:54:13 +0530

    navbar solid effect

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import logo from "../../assets/images/common/camp-k-12-logo.svg";
 import notif from "../../assets/images/common/notif.svg";
 import leaderboard from "../../assets/images/common/leaderboard.svg";
-import { NavContainer } from "./components";
+import { NavContainer, NavLimiter } from "./components";
 
 export default function Navbar() {
   const [navBackground, setNavBackground] = useState(false);
@@ -27,21 +27,23 @@ export default function Navbar() {
 
   return (
     <NavContainer id="header-nav-bar" isSolidBackground={navBackground}>
-      <div>
-        <img src={logo} alt="logo" />
-      </div>
-      <div>
-        <img
-          src={notif}
-          alt="notification icon"
-          style={{ paddingLeft: "1rem" }}
-        />
-        <img
-          src={leaderboard}
-          alt="leaderboard icon"
-          style={{ paddingLeft: "1rem" }}
-        />
-      </div>
+      <NavLimiter>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div>
+          <img
+            src={notif}
+            alt="notification icon"
+            style={{ paddingLeft: "1rem" }}
+          />
+          <img
+            src={leaderboard}
+            alt="leaderboard icon"
+            style={{ paddingLeft: "1rem" }}
+          />
+        </div>
+      </NavLimiter>
     </NavContainer>
   );
 }
--- src/components/Navbar/components.ts
+++ src/components/Navbar/components.ts
@@ -3,18 +3,29 @@ import { Layout } from "antd";
 const { Header } = Layout;
 
 export const NavContainer = styled(Header)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 1;
+  padding-top: 3.188rem;
+  background-image: ${(props: { isSolidBackground: boolean }) =>
+    props.isSolidBackground
+      ? "linear-gradient(82deg, #207cee -3%, #73caf3 119%)"
+      : "linear-gradient(rgba(1,1,1,0))"};
+  background-color: transparent;
+  padding: 0px;
+  height: 6.375rem;
+`;
+
+export const NavLimiter = styled.div`
   max-width: 1205px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  position: fixed;
-  top: 0;
-  z-index: 1;
-  margin-top: 3.188rem;
-  background-color: ${(props: { isSolidBackground: boolean }) =>
-    props.isSolidBackground ? "#000" : "transparent"};
-  padding: 0px;
-  height: 35px;
 `;
