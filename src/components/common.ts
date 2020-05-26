eb41326dde0ea3b0fe4eee9ae4cb4ced88b8d34f
Utkarsh Maharana <utkarshm27@gmail.com>
2020-05-26 06:55:47 +0530

    change in common styles

--- src/components/common.ts
+++ src/components/common.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-image: linear-gradient(to bottom, #eef9ff -10%, #ade1ff);
-  height: 100vh;
   position: relative;
   display: flex;
   flex-direction: column;
-  z-index: -3;
   align-items: center;
 `;
 
 export const PageContainer = styled.div`
   max-width: 1205px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
